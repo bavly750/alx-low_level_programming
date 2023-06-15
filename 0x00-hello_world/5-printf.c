85dad8ac0c5695f243a1c404a39633928c2ecc79
bavly750 <bavly750@gmail.com>
2023-06-15 07:53:14 -0700

    s

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*fuck this shit 
  * bruh 
- * idk man*/
+ * idk man
+ * */
 int main(void)
 {
-	printf(with proper grammar, but the outcome is a piece of art,);
+	printf("with proper grammar, but the outcome is a piece of art,/n");
 	return (0);
 }
