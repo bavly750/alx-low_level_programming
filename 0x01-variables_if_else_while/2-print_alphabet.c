06828cfb9d3ba2af4897c9d7bb74a751491f6b9f
bavly750 <bavly750@gmail.com>
2023-06-16 11:54:14 -0700

    s

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,12 +3,10 @@
 int main(void) {
     char letter;
 
-    // Print the alphabet using putchar
     for (letter = 'a'; letter <= 'z'; letter++) {
         putchar(letter);
     }
 
-    // Print a new line using putchar
     putchar('\n');
 
     return 0;
