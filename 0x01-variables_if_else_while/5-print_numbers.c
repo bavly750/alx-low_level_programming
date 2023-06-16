e5dedcef9561f5ea6e42e9e23ffed65f08cdde40
bavly750 <bavly750@gmail.com>
2023-06-16 12:15:26 -0700

    s

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stido.h>
+
+/*
+ * this is idk
+ * 
+ * idk as well
+ */
+
+int main(void)
+{
+	int a;
+	for (a = 0 ; a < 10; a++)
+{
+	printf("%d", a);
+}
+printf ("\n");
+return (0);
+}
