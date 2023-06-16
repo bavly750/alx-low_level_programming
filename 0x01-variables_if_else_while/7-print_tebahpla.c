66bb98c18b5eae3f2fe988e5b3c34a16f9350f96
bavly750 <bavly750@gmail.com>
2023-06-16 12:24:07 -0700

    s

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char = letter;
+	for (letter = 'z'; 'z' > 'a'; letter--);
+       putchar (letter);	
+
+       putchat ("\n")
+	       return (0);
+}
+
