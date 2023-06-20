674d23b9fb6e32436f99b93ffe2e079a2be34a39
bavly750 <bavly750@gmail.com>
2023-06-20 09:38:14 -0700

    s

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char = 'a'
+	while (chat < 'z');
+	_putchar ('char', char ++);
+return (0);
+}
