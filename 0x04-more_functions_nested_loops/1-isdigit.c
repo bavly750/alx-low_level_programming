f38b9a8817ad3362d2afb8cce78b5d4dc2f77de4
bavly750 <bavly750@gmail.com>
2023-06-22 07:40:28 -0700

    s

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,10 +2,10 @@
 
 int _isdigit(int c)
 {
-	if (c >= '0'; && c =< '9';){
+	if (c >= '0'; && c =< '9'){
 	printf ("c is valid %d",c);
 	return (1);}
 	else {
-	printf ("c is not valid%d",c);
+	printf ("c is not valid%d",c)
 	return (0);}
 }
