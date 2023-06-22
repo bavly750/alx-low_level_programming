e9a50143e323ee1594ca6160c38e8952a1f86f10
bavly750 <bavly750@gmail.com>
2023-06-22 08:34:29 -0700

    s

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
+/**
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
+
 void print_numbers(void)
 {
 	char a;
+
 	for (a = '0'; a <= '9'; a++)
 	{
 	_putchar(a);
 	}
 	_putchar('\n');
-
-
-
 }
